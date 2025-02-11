7ea663298da0ab819902d3f73d82eda3b0bc8488
JorgeRoniel <jorge.infor4g@gmail.com>
2025-02-11 18:10:45 -0300

    feat: Construção completa do Servidor de Pedidos.

--- Livraria/api-gateway/src/main/java/com/ufc/br/api_gateway/controllers/OrderController.java
+++ Livraria/api-gateway/src/main/java/com/ufc/br/api_gateway/controllers/OrderController.java
@@ -2,12 +2,13 @@ package com.ufc.br.api_gateway.controllers;
 
 import com.ufc.br.api_gateway.dto.CreateOrderDTO;
 import com.ufc.br.api_gateway.services.OrderServiceRMI;
+import org.example.models.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/order")
@@ -23,6 +24,19 @@ public class OrderController {
         if(response.equals("Order Created!")){
             return ResponseEntity.ok().build();
         }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<ArrayList<Order>> listOrders(@PathVariable("id") int userId){
+        if(this.serviceRMI.listOrders(userId) == null) return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(this.serviceRMI.listOrders(userId));
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity deleteOrder(@PathVariable("id") int id){
+        String response = this.serviceRMI.deleteOrder(id);
+        if(response.equals("Order Deleted!")) return ResponseEntity.ok().build();
         return ResponseEntity.badRequest().build();
     }
 }
--- Livraria/api-gateway/src/main/java/com/ufc/br/api_gateway/services/OrderServiceRMI.java
+++ Livraria/api-gateway/src/main/java/com/ufc/br/api_gateway/services/OrderServiceRMI.java
@@ -1,11 +1,14 @@
 package com.ufc.br.api_gateway.services;
 
 import org.example.OrderServices;
+import org.example.models.Order;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderServiceRMI {
@@ -21,6 +24,34 @@ public class OrderServiceRMI {
         } catch (Exception e) {
             e.printStackTrace();
             return "Fail";
+        }
+    }
+
+    public ArrayList<Order> listOrders(int userId){
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+
+            OrderServices stub = (OrderServices) registry.lookup("OrderServices");
+
+            ArrayList<Order> response = stub.listOrders(userId);
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String deleteOrder(int id){
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+
+            OrderServices stub = (OrderServices) registry.lookup("OrderServices");
+
+            String response = stub.deleteOrder(id);
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Fail!";
         }
     }
 }
